2f2254f81ccccb2b1de6d45b1be5107ad3eca030
InsonusK <insonus.k@gmail.com>
2020-10-26 22:14:18 +0300

    Init spring

---
+++ src/main/java/HardNightGame/DesanCardGamestatistic/DesanCardGameStatisticApplication.java
@@ -0,0 +1,13 @@
+package HardNightGame.DesanCardGamestatistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesanCardGameStatisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesanCardGameStatisticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HardNightGame/DesanCardGamestatistic/DesanCardGameStatisticApplicationTests.java
@@ -0,0 +1,13 @@
+package HardNightGame.DesanCardGamestatistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesanCardGameStatisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
