a7b09a82ab6e4a3638ede9b1281652097e9c5736
InsonusK <insonus.k@gmail.com>
2020-11-03 03:41:25 +0300

    Config google oauth, begin test

--- src/main/java/HardNightGame/DesanCardGamestatistic/configurations/SecurityConfig.java
+++ src/main/java/HardNightGame/DesanCardGamestatistic/configurations/SecurityConfig.java
@@ -4,15 +4,17 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-
-            http.antMatcher("/**").authorizeRequests()
+        http
+                .antMatcher("/**").authorizeRequests()
+                .antMatchers(new String[]{"/swagger-ui/index.html","/h2-console/**"}).permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
     }
